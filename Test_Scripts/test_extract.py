72697d8f317b92e141ed37ad5fd7d3970be77775
Suresh V <sureshredd@gmail.com>
2024-09-29 22:27:05 +0530

    Modified test_script

--- Test_Scripts/test_extract.py
+++ Test_Scripts/test_extract.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import pytest
 from sqlalchemy import create_engine
+import cx_Oracle
 
 mysql_conn=create_engine('mysql+pymysql://root:Sur_nar26@localhost:3306/etlretailproject')
+orcl_conn=create_engine('oracle+cx_oracle://system:admin@localhost:1521/xe')
 
 def test_sales_data_extract():
     expected_df=pd.read_csv("Input_Data/sales_data.csv")
@@ -27,3 +29,10 @@ def test_supplier_data_extract():
     query="select * from supplier_staging"
     actual_df=pd.read_sql(query,mysql_conn)
     assert actual_df.equals(expected_df),f"Data mismatch between actual and expected supplier_data. Please verify..."
+
+def test_store_data_extract():
+    quer_orcl="select * from store"
+    expected_df=pd.read_sql(quer_orcl,orcl_conn)
+    quer_mysql="select * from store_staging"
+    actual_result=pd.read_sql(quer_mysql,mysql_conn)
+    assert actual_result.equals(expected_df),f"Data mismatch between actual and expected supplier_data. Please verify..."
